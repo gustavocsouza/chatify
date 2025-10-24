564f799b7e13fd4dcc565ae33d5110d9fcbc30c5
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2025-10-23 22:43:50 -0300

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import cookieParser from 'cookie-parser';
+
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
-import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
