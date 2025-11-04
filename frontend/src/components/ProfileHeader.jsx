f57e2f2b7f3eb427eb0aeba61a47413557896d7e
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2025-11-03 23:20:34 -0300

    beginning the sidebar

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -1,6 +1,44 @@
+import { useState, useRef } from "react";
+import { LogOutIcon, VolumeOffIcon, Volume2Icon } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+
 function ProfileHeader() {
+  const { logout, authUser, updateProfile } = useAuthStore();
+  const { isSoundEnabled, toggleSoud } = useChatStore();
+  const { selectedImg, setSelectedImg } = useState(null);
+
+  const fileInputRef = useRef(null);
+
+  function handleImageUpload(e) {}
+
   return (
-    <div>ProfileHeader</div>
+    <div className="p-6 border-b border-slate-700/50 ">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="avatar online">
+            <button 
+              className="size-14 rounded-full overflow-hidden relative group"
+              onClick={() => fileInputRef.current.click()}
+            >
+              <img 
+                src={selectedImg || authUser.profilePic || "/avatar.png"} 
+                alt="User Image" 
+                className="size-full object-cover"
+              />
+            </button>
+            <input 
+              type="file" 
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
