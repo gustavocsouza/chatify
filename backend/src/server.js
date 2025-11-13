e40d2820cce574050234d39695d74e8c66c405d9
Gustavo Cardoso <gustavo.c.souza1991@gmail.com>
2025-11-13 00:40:13 -0300

    online users status implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,10 +9,10 @@ import { connectDB } from './lib/db.js';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
-const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
@@ -35,7 +35,7 @@ if (ENV.NODE_ENV === "production") {
 }
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
